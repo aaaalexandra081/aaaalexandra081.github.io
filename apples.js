9a775f4ffbbc5983a9736886514dea10049550f1
aaaalexandra081 <165249540+aaaalexandra081@users.noreply.github.com>
2024-03-29 16:08:20 +0800

    homework1 apples

---
+++ apples.js
@@ -0,0 +1,71 @@
+function showPoem() {
+     let poemNumber = document.getElementById('poemNumber').value;
+     let poemContent = document.getElementById('poemContent');
+     if (poemNumber === '1') {
+         poemContent.innerText = '（一）\n苹果能去的地方我永远也去不了都是你的错';
+     } else if (poemNumber === '2') {
+         poemContent.innerText = '（二）\n最小的森林在苹果里 \n最小的风也在苹果里 \n最小的大海在苹果里 \n最小声的呼唤 \n也在苹果里 \n对送你苹果的那个人 \n要说：谢谢';
+       } else if (poemNumber === '3') {
+           poemContent.innerText = '（三）\n街道拐弯处第三家水果店 \n经过一万遍也不曾扭头看一眼整个冬天过去了 \n左边靠着桔子 \n右边是葡萄 \n你故意没看到 \n说一万遍“对不起” \n也没有用了';}
+           else if (poemNumber === '4') {
+               poemContent.innerText = '（四）\n苹果才不害怕孤独 \n苹果籽簇在一起呼呼大睡 \n苹果挂在树上 \n恒星被星系环绕 \n星云后面是黑洞 \n流星雨决定转道降临地球 \n那时苹果还没落地 \n世界还没有地心引力';}
+               else if (poemNumber === '5') {
+                   poemContent.innerText = '（五）\n苹果消失于一场奔跑之中脚踵间的风呼啦啦经过苹果苹果就越来越小 \n越来越小';}
+                   else if (poemNumber === '6') {
+                       poemContent.innerText = '（六）\n苹果诞生于告别之中 \n说过一切告别的话后 \n再无话可说 \n苹果只好发芽';}
+                       else if (poemNumber === '7') {
+                           poemContent.innerText = '（七）\n有一块苹果永远噎在喉咙里有一个人一生都想吐出它谁都知道苹果红的那面有毒却不知青的那面难吃';}
+                           else if (poemNumber === '8') {
+                               poemContent.innerText = '（八）\n苹果的流浪史 \n一万年也说不完 \n是因为 \n他说的时候 \n一直在回避重点';
+                             } else if (poemNumber === '9') {
+                                 poemContent.innerText = '（九）\n而你回避苹果 \n像说谎的人回避直视的目光';}
+                                 else if (poemNumber === '10') {
+                                     poemContent.innerText = '（十）\n好吧 \n如果生下的是女孩 \n就给她取名苹果 \n你说过会娶她 \n婚礼上将到齐她所有的爱人';}
+                                     else if (poemNumber === '11') {
+                                         poemContent.innerText = '（十一）\n过去的悲伤与欢乐全都到此为止 \n但苹果悄悄告诉了你 \n它们又将从哪里另外开始';}
+                                         else if (poemNumber === '12') {
+                                             poemContent.innerText = '（十二）\n那么快去把那个摘苹果的女子抱来放在膝上看她怎么死亡';}
+                                             else if (poemNumber === '13') {
+                                                 poemContent.innerText = '（十三）\n除了苹果 \n谁还会在一万年后仍惦记你 \n谁还会在一万年后 \n仍耿耿于怀那人对你的抛弃 \n除了苹果 \n谁还能替你走到他的对面 \n继续替你索取他的爱意';}
+                                                 else if (poemNumber === '14') {
+                                                     poemContent.innerText = '（十四）\n弥留之际 \n曾对苹果说出唯一的要求 \n使用的却是苹果无论如何 \n也听不懂的语言';}
+                                                     else if (poemNumber === '15') {
+                                                         poemContent.innerText = '（十五）\n事情的经过是这样的： \n你的河漂来苹果时 \n你正在熟睡 \n你的河 \n为你拐了一道弯 \n又一道弯 \n再一道弯 \n苹果接近你 \n又接近你 \n再接近你 \n然后永远离你而去';}
+                                                         else if (poemNumber === '16') {
+                                                             poemContent.innerText = '（十六）\n事情的结局是这样的：苹果搁浅在沙滩上 \n你的河流进大海';}
+                                                             else if (poemNumber === '17') {
+                                                                 poemContent.innerText = '（十七）\n苹果说 \n你在岸边熟睡 \n容貌使人落泪';}
+                                                                 else if (poemNumber === '18') {
+                                                                     poemContent.innerText = '（十八）\n有时候 \n面对一只苹果 \n全世界能让你依靠的只有你椅子的靠背';}
+                                                                     else if (poemNumber === '19') {
+                                                                         poemContent.innerText = '（十九）\n你试着啃食苹果 \n触着的却是皮肤';}
+                                                                         else if (poemNumber === '20') {
+                                                                             poemContent.innerText = '（二十）\n苹果给你指引的路 \n令你越走越远 \n从此与苹果永不相交永不见面';}
+                                                                             else if (poemNumber === '21') {
+                                                                                 poemContent.innerText = '（二十一）\n谁仍在固执地走向苹果 \n纵然明知 \n从此走向的是无边荒野';}
+                                                                                 else if (poemNumber === '22') {
+                                                                                     poemContent.innerText = '（二十二）\n对给你苹果的人 \n请说：再见';}
+                                                                                     else if (poemNumber === '23') {
+                                                                                         poemContent.innerText = '（二十三）\n苹果的第八个故事： \n她在人群中看你的时候 \n全世界都在看你';}
+                                                                                         else if (poemNumber === '24') {
+                                                                                             poemContent.innerText = '（二十四）\n苹果的第四百五十六个故事： \n白头发老爷爷说 \n少年时眼看飞碟渐渐离去 \n为了爱情选择成为地球人 \n白头发老奶奶说 \n一个月弄丢三把雨伞 \n立刻给我滚回外星！';}
+                                                                                             else if (poemNumber === '25') {
+                                                                                                 poemContent.innerText = '（二十五）\n苹果的第一千零一个故事： \n她曾满携欢乐 \n向他而来 \n他也曾徙步千里 \n前去迎她 \n但是一百年过去了 \n他们仍不能相遇 \n这片大地是多么广阔啊！';}
+                                                                                                 else if (poemNumber === '26') {
+                                                                                                     poemContent.innerText = '（二十六）\n苹果的第两万三千八百零七个故事：她把眼泪收集起来 \n但她不流出 \n但她会为他遥指一个湖泊';}
+                                                                                                     else if (poemNumber === '27') {
+                                                                                                         poemContent.innerText = '（二十七）\n苹果的第十万六千个故事： \n有人准备好了最珍爱的一切 \n随你去挥霍';}
+                                                                                                         else if (poemNumber === '28') {
+                                                                                                             poemContent.innerText = '（二十八）\n苹果的第三百八十二万个故事： \n雪停的时候才想起 \n忘了给他留下一行脚印 \n后来他去找她 \n找到一片雪地 \n就断了线索';}
+                                                                                                             else if (poemNumber === '29') {
+                                                                                                                 poemContent.innerText = '（二十九）\n苹果的第三百八十九万零一个故事：直到哭化了 \n才发现自己其实是个雪人';}
+                                                                                                                 else if (poemNumber === '30') {
+                                                                                                                     poemContent.innerText = '（三十）\n苹果的第一千四百六十六万个故事： \n她曾和他的姊妹一样 \n她曾和他的母亲一样 \n她曾和他的妻子一样 \n她曾 \n和他一样';}
+                                                                                                                     else if (poemNumber === '31') {
+                                                                                                                         poemContent.innerText = '（三十一）\n苹果的最后一个故事： \n最后 \n我成了一棵苹果树';}
+                                                                                                                         else if (poemNumber === '32') {
+        poemContent.innerText = '（三十二）\n苹果的第一个故事：没有选择 \n我们必须恋爱';
+    } else {
+        poemContent.innerText = '没有那么多苹果，谢谢。';
+    }
+}
